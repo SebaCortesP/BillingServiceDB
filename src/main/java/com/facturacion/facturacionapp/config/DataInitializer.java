cfada7512af8ca970cfbbf99a6b4edff7f4bd8ef
SebaCortesP <seba.cortesp@duocuc.cl>
2025-05-01 15:07:30 -0400

    docker init

--- src/main/java/com/facturacion/facturacionapp/config/DataInitializer.java
+++ src/main/java/com/facturacion/facturacionapp/config/DataInitializer.java
@@ -23,38 +23,48 @@ import java.util.List;
 @Configuration
 public class DataInitializer {
         @Bean
-        CommandLineRunner initData(VeterinaryServiceRepository serviceRepo, ClientRepository clientRepository, BillRepository billRepo) {
-                log.info("STARTING INIT");
-                return args -> { 
-                        log.info("ATATATATATATA");                         
-                        Client c1 = new Client(null, "Fabian Nitan Ayuda", "Calle 123", "123456789");
-                        Client c2 = new Client(null, "Rodrigo Guerrero", "Av. Libertador 456", "987654321");
-                        Client c3 = new Client(null, "Pepe Tapia", "Calle Falsa 123", null);
-                        Client c4 = new Client(null, "Sebastian Soto", "Ruta 40", "555555555");
-                        Client c5 = new Client(null, "Braulio Perez", "Calle Sarmiento", null);
-                        clientRepository.saveAll(List.of(c1, c2, c3, c4, c5));            
-                        System.out.println("Clientes cargados exitosamente");
-                        // Crear Servicios
-                        VeterinaryService s1 = new VeterinaryService(null, "Consulta general", "Revisión completa", 20000);
-                        VeterinaryService s2 = new VeterinaryService(null, "Vacunación", "Aplicación de vacunas", 15000);
-                        VeterinaryService s3 = new VeterinaryService(null, "Desparasitación", "Eliminación de parásitos", 10000);
-                        VeterinaryService s4 = new VeterinaryService(null, "Radiografía", "Diagnóstico por imagen", 30000);
-                        VeterinaryService s5 = new VeterinaryService(null, "Cirugía menor", "Intervención quirúrgica", 50000);
-                        serviceRepo.saveAll(List.of(s1, s2, s3, s4, s5));
-                        Bill bill1 = new Bill();
-                        bill1.setCliente(c1);
-                        bill1.setPayed(false);
-                        BillDetail d1 = new BillDetail(s1, 1); 
-                        BillDetail d2 = new BillDetail(s2, 2); 
-                        bill1.addDetail(d1);
-                        bill1.addDetail(d2);
-                        // Otra factura para Ana López
-                        Bill bill2 = new Bill();
-                        bill2.setCliente(c2);
-                        bill2.setPayed(true);
-                        BillDetail d3 = new BillDetail(s5, 1); 
-                        bill2.addDetail(d3);
-                        billRepo.saveAll(List.of(bill1, bill2));
-                };   
-        }       
+        CommandLineRunner initData(
+            VeterinaryServiceRepository serviceRepo,
+            ClientRepository clientRepository,
+            BillRepository billRepo
+        ) {
+            return args -> {
+                if (clientRepository.count() > 0) {
+                    System.out.println("Ya existen datos. Saltando inicialización.");
+                    return;
+                }
+        
+                System.out.println("Inicializando datos...");
+        
+                Client c1 = new Client(null, "Fabian Nitan Ayuda", "Calle 123", "123456789");
+                Client c2 = new Client(null, "Rodrigo Guerrero", "Av. Libertador 456", "987654321");
+                Client c3 = new Client(null, "Pepe Tapia", "Calle Falsa 123", null);
+                Client c4 = new Client(null, "Sebastian Soto", "Ruta 40", "555555555");
+                Client c5 = new Client(null, "Braulio Perez", "Calle Sarmiento", null);
+                clientRepository.saveAll(List.of(c1, c2, c3, c4, c5));
+        
+                VeterinaryService s1 = new VeterinaryService(null, "Consulta general", "Revisión completa", 20000);
+                VeterinaryService s2 = new VeterinaryService(null, "Vacunación", "Aplicación de vacunas", 15000);
+                VeterinaryService s3 = new VeterinaryService(null, "Desparasitación", "Eliminación de parásitos", 10000);
+                VeterinaryService s4 = new VeterinaryService(null, "Radiografía", "Diagnóstico por imagen", 30000);
+                VeterinaryService s5 = new VeterinaryService(null, "Cirugía menor", "Intervención quirúrgica", 50000);
+                serviceRepo.saveAll(List.of(s1, s2, s3, s4, s5));
+        
+                Bill bill1 = new Bill();
+                bill1.setCliente(c1);
+                bill1.setPayed(false);
+                bill1.addDetail(new BillDetail(s1, 1));
+                bill1.addDetail(new BillDetail(s2, 2));
+        
+                Bill bill2 = new Bill();
+                bill2.setCliente(c2);
+                bill2.setPayed(true);
+                bill2.addDetail(new BillDetail(s5, 1));
+        
+                billRepo.saveAll(List.of(bill1, bill2));
+        
+                System.out.println("Datos iniciales cargados.");
+            };
+        }
+             
 }
